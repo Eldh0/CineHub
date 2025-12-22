7e5546e9b2e8e802425cde9bbf0c84729d4850c1
Eldh0 <eldhojoy124@gmail.com>
2025-12-22 13:49:44 +0530

    setup routing in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Movies } from './pages/movies/movies';
+import { Shows } from './pages/shows/shows';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:Home
+    },
+    {
+        path: 'movies',
+        component:Movies
+    },
+    {
+        path: 'tv-shows',
+        component:Shows
+    }
+];
+ 
