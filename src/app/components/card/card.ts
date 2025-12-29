4ecbb4e0ef371b846b46744629d7cc4e361eb881
Eldh0 <eldhojoy124@gmail.com>
2025-12-29 22:44:10 +0530

    update card to display api contents

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './card.html',
   styleUrl: './card.css',
 })
 export class Card {
+
+ @Input() item: any;
+  constructor(private router:Router) {}
+ viewMediaDetails(item:any) {
+    this.router.navigate(['/media'],{state:{item}});
+  }
 
 }
