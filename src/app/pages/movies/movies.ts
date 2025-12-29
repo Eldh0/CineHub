6b36effdbd6a306ab6e41c98dffab6b68b31cf28
Eldh0 <eldhojoy124@gmail.com>
2025-12-29 19:33:35 +0530

    pass type when loading carousel component

--- src/app/pages/movies/movies.ts
+++ src/app/pages/movies/movies.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Carousel } from "../../components/carousel/carousel";
+import { Card } from "../../components/card/card";
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [Carousel, Card],
   templateUrl: './movies.html',
   styleUrl: './movies.css',
 })
--- src/app/pages/shows/shows.ts
+++ src/app/pages/shows/shows.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Carousel } from "../../components/carousel/carousel";
 
 @Component({
   selector: 'app-shows',
-  imports: [],
+  imports: [Carousel],
   templateUrl: './shows.html',
   styleUrl: './shows.css',
 })
