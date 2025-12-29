3d5c0c7e4ed3c6b30d531df911006656035a5d17
Eldh0 <eldhojoy124@gmail.com>
2025-12-29 19:32:36 +0530

    update carousel to fetch different api

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit, SimpleChanges,OnChanges } from '@angular/core';
+import { ApiService } from '../../api-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './carousel.html',
   styleUrl: './carousel.css',
 })
-export class Carousel {
+export class Carousel{
+  items: any[] = [];
+
+  @Input() set type(value: 'movies' | 'tv') {
+    this.loadItems(value)
+  }
+  
+  constructor(private api: ApiService, private cdr: ChangeDetectorRef) { }
+
+  private loadItems(Type:string) {
+   const request = Type === 'movies' 
+    ? this.api.getPopularMovies() 
+    : this.api.getPopularTvShows();
+
+  request.subscribe((res: any) => {
+    this.items = res.results.slice(0, 6);
+    this.cdr.detectChanges(); 
+  });
+  }
 
 }
