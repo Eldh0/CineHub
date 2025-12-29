91e82d74167e2d46ac46143f5f40690cb8510fe8
Eldh0 <eldhojoy124@gmail.com>
2025-12-29 22:23:30 +0530

    update tv and movies cards with api details and image

--- src/app/pages/movies/movies.ts
+++ src/app/pages/movies/movies.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Carousel } from "../../components/carousel/carousel";
 import { Card } from "../../components/card/card";
+import { ApiService } from '../../api-service';
 
 @Component({
   selector: 'app-movies',
@@ -8,6 +9,35 @@ import { Card } from "../../components/card/card";
   templateUrl: './movies.html',
   styleUrl: './movies.css',
 })
-export class Movies {
+export class Movies implements OnInit {
+  movieItems: any[] = [];
+  englishMovieItems: any[] = [];
+  romanceComedyItems: any[] = []
+  sciFiItems: any[] = [];
 
+  constructor(private api: ApiService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.api.getTopRated().subscribe((res: any) => {
+        this.movieItems = res.results.slice(0, 15);
+        this.cdr.detectChanges(); 
+      });
+
+    this.api.getEnglishMovies().subscribe((res: any) => {
+        this.englishMovieItems = res.results.slice(0, 15);
+        this.cdr.detectChanges(); 
+      });
+    this.api.getRomanticComedy().subscribe((res: any) => {
+        this.romanceComedyItems = res.results.slice(0, 15);
+        this.cdr.detectChanges(); 
+      });
+    this.api.getSciFic().subscribe((res: any) => {
+        this.sciFiItems = res.results.slice(0, 15);
+        this.cdr.detectChanges(); 
+      });
+  }
 }
--- src/app/pages/shows/shows.ts
+++ src/app/pages/shows/shows.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Carousel } from "../../components/carousel/carousel";
+import { Card } from "../../components/card/card";
+import { ApiService } from '../../api-service';
 
 @Component({
   selector: 'app-shows',
-  imports: [Carousel],
+  imports: [Carousel, Card],
   templateUrl: './shows.html',
   styleUrl: './shows.css',
 })
 export class Shows {
 
-}
+  tvItems: any[] = [];
+  romanceTvItems: any[] = [];
+  comedyTvItems: any[] = []
+  sciFiTvItems: any[] = [];
+
+  constructor(private api: ApiService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.getTvShows();
+  }
+
+  private getTvShows() {
+    this.api.getTopRatedTvShows().subscribe((res: any) => {
+        this.tvItems = res.results.slice(0, 15);
+        this.cdr.detectChanges(); 
+      });
+    this.api.getRomanceTvShows().subscribe((res: any) => {
+        this.romanceTvItems = res.results.slice(0, 12);
+        this.cdr.detectChanges(); 
+      });
+    
+      this.api.getComedyTvShows().subscribe((res: any) => {
+        this.comedyTvItems = res.results.slice(0, 12);
+        this.cdr.detectChanges(); 
+      });
+
+      this.api.getAnimeTvShows().subscribe((res: any) => {  
+        this.sciFiTvItems = res.results.slice(0, 12);
+        this.cdr.detectChanges(); 
+      });
+    }    
+  }
