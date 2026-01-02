fb95cab4e9df640d8cf6e05f02049191cf999f5e
Eldh0 <eldhojoy124@gmail.com>
2026-01-02 21:20:22 +0530

    add:page not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './pages/home/home';
 import { Movies } from './pages/movies/movies';
 import { Shows } from './pages/shows/shows';
 import { Media } from './pages/media/media';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,10 @@ export const routes: Routes = [
     {
         path:'media/:type/:id',
         component:Media
+    },
+    {
+        path: '**',
+        component:PageNotFound
     }
 ];
  
